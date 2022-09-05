ec48e1e32f3d53c21b419908a474605372c9371a
Meryem Sel <mzbasel637936@gmail.com>
2022-09-05 11:42:56 +0100

    LeapYear

--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -1,11 +1,16 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LeapYearTest {
     @Test
     void is_false_if_not_dividable_by_four() {
         assertFalse(LeapYear.check(1997));
+    }
+    @Test
+    void is_true_if_dividable_by_four() {
+        assertTrue(LeapYear.check(2000));
     }
 
 }
